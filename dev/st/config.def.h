e12e53e8e3b25b0113473f128fdf3c40a17d315a
Cedric <cedric.todaro@gmail.com>
2024-10-21 20:22:17 +0200

    lun. 21 oct. 2024 20:22:14 CEST

--- dev/st/config.def.h
+++ dev/st/config.def.h
@@ -105,41 +105,62 @@ unsigned int tabspaces = 8;
 static const char* colorname[] = {
     /* 8 normal colors */
     //  [0] = "#282a2e", /* black   */
-    [0] = "#191223", /* black   */
-                     //  [0] = "#000000", /* black   */
-    [1] = "#a54242", /* red     */
-    [2] = "#8c9440", /* green   */
-    [3] = "#de935f", /* yellow  */
-    [4] = "#5f819d", /* blue    */
-    [5] = "#85678f", /* magenta */
-    [6] = "#5e8d87", /* cyan    */
-    [7] = "#707880", /* white   */
+    // [0] = "#191223", /* black   */
+    // [0] = "#000000", /* black   */
+    // [1] = "#a54242", /* red     */
+    // [2] = "#8c9440", /* green   */
+    // [3] = "#de935f", /* yellow  */
+    // [4] = "#5f819d", /* blue    */
+    // [5] = "#85678f", /* magenta */
+    // [6] = "#5e8d87", /* cyan    */
+    // [7] = "#707880", /* white   */
 
     /* 8 bright colors */
-    [8] = "#373b41",  /* black   */
-    [9] = "#cc6666",  /* red     */
-    [10] = "#b5bd68", /* green   */
-    [11] = "#f0c674", /* yellow  */
-    [12] = "#81a2be", /* blue    */
-    [13] = "#b294bb", /* magenta */
-    [14] = "#8abeb7", /* cyan    */
-    [15] = "#c5c8c6", /* white   */
+    // [8] = "#373b41",  /* black   */
+    // [9] = "#cc6666",  /* red     */
+    // [10] = "#b5bd68", /* green   */
+    // [11] = "#f0c674", /* yellow  */
+    // [12] = "#81a2be", /* blue    */
+    // [13] = "#b294bb", /* magenta */
+    // [14] = "#8abeb7", /* cyan    */
+    // [15] = "#c5c8c6", /* white   */
 
     /* special colors */
-    //[256] = "#1d1f21", /* background */
+    // [256] = "#1d1f21", /* background */
     // [256] = "#191223", /* background */
-    [256] = "#000000", /* background */
-    //[256] = "#010309", /* background */
-    [257] = "#c5c8c6", /* foreground */
+    // [256] = "#000000", /* background */
+    // [256] = "#010309", /* background */
+    // [257] = "#c5c8c6", /* foreground */
+
+   	// [0] = "#1d2021", /* hard contrast: #1d2021 / soft contrast: #32302f */
+   	[0] = "#000000", /* hard contrast: #1d2021 / soft contrast: #32302f */
+	[1] = "#cc241d", /* red     */
+	[2] = "#98971a", /* green   */
+	[3] = "#d79921", /* yellow  */
+	[4] = "#458588", /* blue    */
+	[5] = "#b16286", /* magenta */
+	[6] = "#689d6a", /* cyan    */
+	[7] = "#989984", /* white   */
+	[8]  = "#928374", /* black   */
+	[9]  = "#fb4934", /* red     */
+	[10] = "#b8bb26", /* green   */
+	[11] = "#fabd2f", /* yellow  */
+	[12] = "#83a598", /* blue    */
+	[13] = "#d3869b", /* magenta */
+	[14] = "#8ec07c", /* cyan    */
+	[15] = "#ffffff", /* white   */
 };
 
 /*
  * Default colors (colorname index)
  * foreground, background, cursor
  */
-unsigned int defaultfg = 257;
-unsigned int defaultbg = 256;
-unsigned int defaultcs = 257;
+unsigned int defaultfg = 15;
+unsigned int defaultbg = 0;
+unsigned int defaultcs = 15;
+// unsigned int defaultfg = 257;
+// unsigned int defaultbg = 256;
+// unsigned int defaultcs = 257;
 static unsigned int defaultrcs = 257;
 
 /*
