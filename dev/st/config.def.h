61cc9e79c18f85a762a54a6f62f8f38c198ac7fd
Cedric <cedric.todaro@gmail.com>
2024-11-06 06:56:32 +0100

    mer. 06 nov. 2024 06:56:14 CET

--- dev/st/config.def.h
+++ dev/st/config.def.h
@@ -6,7 +6,7 @@
  */
 // static char *font = "Hack:size=12";
 // static char *font = "IBM Plex Mono:size=14";
-static char *font = "Firacode Nerd Font Mono:size=14";
+static char *font = "Firacode Nerd Font Mono:size=16";
 // static char* font = "SauceCodePro Nerd Font Mono:size=14";
 // static char *font = "terminus:pixelsize=18:antialias=true:autohint=true";
 // static char *font = "agave:size=10";
